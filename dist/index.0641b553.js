87b381a950e4901161cf0d1810446ab219c8a412
Pierre-Antoine Tremblay <pierre-antoinetremblay@MacBook-Pro-de-Pierre-Antoine.local>
2022-11-20 14:58:42 +0100

    big update 3

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -532,8 +532,34 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"bNKaB":[function(require,module,exports) {
-const greeting = "horn players from around the world!";
-console.log(`Hello ${greeting}`);
+myFunction("france-selector", "french");
+myFunction("germany-selector", "german");
+myFunction("spain-selector", "spanish");
+myFunction("us-selector", "american");
+myFunction("uk-selector", "english");
+myFunction("other-selector", "other");
+function myFunction(selector, nationality) {
+    const countrySelector = Array.from(document.getElementsByClassName(selector));
+    countrySelector.forEach((selector2)=>{
+        selector2.addEventListener("click", function handleClick(event) {
+            resetSelectorColor();
+            const hornists = Array.from(document.getElementsByClassName("hornist"));
+            hornists.forEach((hornist)=>{
+                if (hornist.className.indexOf(nationality) < 0) hornist.setAttribute("style", "display: none;");
+                else {
+                    hornist.setAttribute("style", "display: list-item;");
+                    selector2.setAttribute("style", "background: var(--primary-color2);");
+                }
+            });
+        });
+    });
+}
+function resetSelectorColor() {
+    const countrySelector = Array.from(document.getElementsByClassName("item"));
+    countrySelector.forEach((item)=>{
+        item.setAttribute("style", "background: var(--primary-color);");
+    });
+}
 
 },{}]},["7Aums","bNKaB"], "bNKaB", "parcelRequirec240")
 
