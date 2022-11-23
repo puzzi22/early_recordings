1bc4297de0f9e4e4b71ce3fdd437e8d41725a402
Pierre-Antoine Tremblay <pierre-antoinetremblay@MacBook-Pro-de-PA.local>
2022-11-23 15:22:04 +0100

    build prod 1

--- dist/countries.a2362d03.js
+++
@@ -1,7 +0,0 @@
-function toggleMobileNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") x.className += " responsive";
-    else x.className = "topnav";
-}
-
-//# sourceMappingURL=countries.a2362d03.js.map
---
+++ dist/index.57c2bf34.js
@@ -0,0 +1,2 @@
+function toggleMobileNav(){var a=document.getElementById("myTopnav");"topnav"===a.className?a.className+=" responsive":a.className="topnav"}
+//# sourceMappingURL=index.57c2bf34.js.map
