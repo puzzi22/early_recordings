a820cddbfe2394541f6b1c83dd64bf023d08ac8f
Pierre-Antoine Tremblay <pierre-antoinetremblay@MacBook-Pro-de-PA.local>
2022-11-24 10:43:06 +0100

    build prod 5 corrections markup

--- dist/description.03e56de7.js
+++
@@ -1,2 +0,0 @@
-function toggleMobileNav(){var a=document.getElementById("myTopnav");"topnav"===a.className?a.className+=" responsive":a.className="topnav"}
-//# sourceMappingURL=description.03e56de7.js.map
